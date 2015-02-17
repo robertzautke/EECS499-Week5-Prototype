c651de6be2083b749a2ba5d26625cac2a6fc3df6
rzautke <rzautke@umich.edu>
2015-02-16 23:05:17 -0500

    initial push.

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+
+
+public class GameController : MonoBehaviour
+{
+
+	public GameObject spellingBox;
+	public int lives = 5;
+	public int tempLives = 5;
+	public int correct = 0;
+	public List<Word> dictionary = new List<Word>();
+	public List<Word> missedWords = new List<Word>();
+
+	public float generationTimer = 0f;
+	public float waitTime = 3f;
+
+	int selectableCount = 0;
+
+	void Start()
+	{
+		Populate(dictionary);
+		generationTimer = 0f;
+		tempLives = lives;
+	}
+
+	void Update()
+	{
+		generationTimer += Time.deltaTime;
+
+		if (generationTimer >= waitTime)
+		{
+			generationTimer = 0f;
+			//generate new SpellingBox
+			int index = Random.Range(0, dictionary.Count);
+			GameObject newSpellingBox = Instantiate(spellingBox) as GameObject;
+			newSpellingBox.GetComponent<SpellingBox>().word = dictionary[index];
+			newSpellingBox.transform.SetParent(GameObject.Find("MainCanvas").transform);
+
+			Vector3 temp = new Vector3(Screen.width / 2f, Screen.height, 0);
+			temp.x = Random.Range((Screen.width / 4f), 3 * (Screen.width / 4f));
+			newSpellingBox.GetComponent<RectTransform>().position = temp;
+
+		}
+
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			selectableCount -= 1;
+			if (selectableCount < 0)
+			{
+				selectableCount = Selectable.allSelectables.Count - 1;
+			}
+			Selectable.allSelectables[selectableCount].Select();
+		}
+
+		if(tempLives != lives)
+		{ 
+			tempLives = lives;
+			//for(int i = 0; i < missedWords.Count; i++)
+				GameObject.FindGameObjectWithTag("MissedWords").GetComponent<Text>().text += "\n" + missedWords[missedWords.Count - 1].correctSpelling;
+		}
+	}
+
+	void Populate(List<Word> inDictionary)
+	{
+
+		TextAsset txt = Resources.Load("wordlist") as TextAsset;
+		StringReader content = new StringReader(txt.text);
+		string line = content.ReadLine();
+		while (line != null)
+		{
+			string[] words = line.Split(' ');
+			dictionary.Add(new Word(words[0], words[1]));
+			line = content.ReadLine();
+		}
+
+	}
+}
+
+public class Word
+{
+	public string correctSpelling;
+	public string incorrectSpelling;
+	public Word(string inputCorrectString, string inputIncorrectString)
+	{
+		correctSpelling = inputCorrectString;
+		incorrectSpelling = inputIncorrectString;
+	}
+}
---
+++ Assets/Scripts/SpellingBox.cs
@@ -0,0 +1,73 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+
+public class SpellingBox : MonoBehaviour
+{
+
+	public GameObject lives;
+	public GameObject correct;
+	public float correctSpellingChance = 0.5f;
+	public float downwardMovementSpeed = 1f;
+	public string answer;
+	public string inputAnswer;
+	public Word word;
+
+	private float timer = 0f;
+
+
+	void Start()
+	{
+		timer = 0f;
+		answer = word.correctSpelling;
+
+		lives = GameObject.FindGameObjectWithTag("Lives");
+		correct = GameObject.FindGameObjectWithTag("Correct");
+
+		if (Random.Range(0f, 1f) < correctSpellingChance)
+		{
+			this.transform.FindChild("Placeholder").GetComponent<Text>().text = word.correctSpelling;
+		}
+		else
+		{
+			this.transform.FindChild("Placeholder").GetComponent<Text>().text = word.incorrectSpelling;
+		}
+	}
+
+	void Update()
+	{
+		timer += Time.deltaTime;
+
+		if (transform.position.y < -15)
+		{
+			if (transform.Find("InputText").GetComponent<Text>().text == "")
+			{
+				inputAnswer = transform.Find("Placeholder").GetComponent<Text>().text;
+			}
+			else
+			{
+				inputAnswer = transform.Find("InputText").GetComponent<Text>().text;
+			}
+
+			if (inputAnswer == answer)
+			{
+				GameObject.Find("GameController").GetComponent<GameController>().correct += 1;
+				this.correct.GetComponent<Text>().text = "Correct: " + GameObject.Find("GameController").GetComponent<GameController>().correct;
+			}
+			else
+			{
+				GameObject.Find("GameController").GetComponent<GameController>().lives -= 1;
+				this.lives.GetComponent<Text>().text = "Lives: " + GameObject.Find("GameController").GetComponent<GameController>().lives;
+				GameObject.Find("GameController").GetComponent<GameController>().missedWords.Add(new Word(word.correctSpelling, inputAnswer));
+			}
+
+			Destroy(this.gameObject);
+		}
+
+		// Move SpellingBox down slowly
+		Vector3 currentPosition = transform.position;
+		currentPosition = new Vector3(currentPosition.x, currentPosition.y - downwardMovementSpeed, currentPosition.z);
+		transform.position = currentPosition;
+	}
+}
